1aacd3c6f1bf8396e157116db5dfb46deeaadf1b
notS0LD13R <joelsebin@gmail.com>
2023-06-06 17:02:07 +0530

    Added TodoSearch component Ui

---
+++ FrontEnd/src/Components/Todo/SubComps/SearchBar.tsx
@@ -0,0 +1,13 @@
+import './SearchBar.scss'
+
+
+export default function SearchBar() {
+  
+  
+  return (
+    <div className="searchbar">
+      <input type="text" placeholder='Enter Task'/>
+      <input type="submit" value=""  />
+    </div>
+  )
+}
